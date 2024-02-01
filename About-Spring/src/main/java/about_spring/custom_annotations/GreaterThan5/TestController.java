21c6d78e24bc715c2fb9ce0da0e62f5029ab0753
Petar <petar.kostadinovski@intertec.io>
2024-02-01 10:26:09 +0100

    add test

--- About-Spring/src/main/java/about_spring/custom_annotations/GreaterThan5/TestController.java
+++ About-Spring/src/main/java/about_spring/custom_annotations/GreaterThan5/TestController.java
@@ -12,4 +12,9 @@ public class TestController {
 
     }
 
+    @GetMapping
+    void test() {
+
+    }
+
 }
---
+++ About-Spring/src/test/java/about_spring/TestControllerIT.java
@@ -0,0 +1,36 @@
+package about_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.net.URI;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        classes = AboutSpringApplication.class
+)
+public class TestControllerIT {
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Test
+    void test() {
+        ResponseEntity<Void> response = testRestTemplate.exchange(
+                URI.create("/api/test"),
+                HttpMethod.GET,
+                null,
+                Void.class
+        );
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+}
